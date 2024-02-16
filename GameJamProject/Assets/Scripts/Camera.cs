e90e03260b4ff4222513530e3cb8cb6fd92ee04d
MUKESHK243 <mukesh.kumaran@outlook.com>
2024-02-16 09:47:11 +0530

    added 3rd person controller

--- GameJamProject/Assets/Scripts/Camera.cs
+++ GameJamProject/Assets/Scripts/Camera.cs
@@ -5,20 +5,20 @@ using UnityEngine.Windows;
 
 public class Camera : MonoBehaviour
 {
-    public Transform player;
-    public GameObject cameraConstraint;
-    public Vector3 offset;
-    public float chasingSpeed;
-    // Start is called before the first frame update
-    void Start()
-    {
+    //public Transform player;
+    //public GameObject cameraConstraint;
+    //public Vector3 offset;
+    //public float chasingSpeed;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
         
-    }
+    //}
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        transform.position = Vector3.Lerp(transform.position, player.position + player.TransformVector(offset), chasingSpeed * Time.deltaTime);
-        transform.LookAt(player);
-    }
+    //// Update is called once per frame
+    //void FixedUpdate()
+    //{
+    //    transform.position = Vector3.Lerp(transform.position, player.position + player.TransformVector(offset), chasingSpeed * Time.deltaTime);
+    //    transform.LookAt(player);
+    //}
 }
