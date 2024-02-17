623803b9bdd25b8065b969244d0d125d697e2c2e
MUKESHK243 <mukesh.kumaran@outlook.com>
2024-02-17 22:14:14 +0530

    Mini Map Added

---
+++ GameJamProject/Assets/Scripts/MiniMap.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMap : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    private void LateUpdate()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+
+        transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+    }
+    //private void Update()
+    //{
+    //    transform.position = player.position + offset;
+    //}
+}
