8eecab20afdbdba3e6fd527b8a0d1b1896634a8f
MUKESHK243 <mukesh.kumaran@outlook.com>
2024-02-17 09:01:38 +0530

    Weapon and New character asset added

--- GameJamProject/Assets/Scripts/PlayerController.cs
+++ GameJamProject/Assets/Scripts/PlayerController.cs
@@ -4,39 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header ("Component References")]
-    public Animator playerAnim;
-
-    [Header("Inputs")]
-    private float horizontalInput;
-    private float verticalInput;
-
-    [Header ("Variables")]
-    public float speed;
-    public float rotationSpeed;
+    public GameObject sword;
     // Start is called before the first frame update
     void Start(){
-       playerAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update(){
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical"); 
-
-        MovePlayer();
-        PlayerAnimations();
-    }
-
-    void MovePlayer(){
-        //To move player
-        transform.Translate(Vector3.forward * speed * verticalInput * Time.deltaTime);
-        
-        //To rotate player
-        transform.Rotate(Vector3.up * rotationSpeed * horizontalInput * Time.deltaTime);
-    }
-
-    void PlayerAnimations(){
         
     }
+
+    
 }
---
+++ GameJamProject/Assets/Scripts/Weapon.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    PlayerController controller;
+    public bool isEquiped = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            isEquiped=true;
+            gameObject.SetActive(false);
+            controller.sword.SetActive(true);
+
+        }
+    }
+}
