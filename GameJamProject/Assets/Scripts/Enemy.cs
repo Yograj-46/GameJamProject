82334f9c02f75856f47b51f47a9d84e2009770e5
Yograj-46 <imyograj.dec15@gmail.com>
2024-02-22 16:35:44 +0530

    Added a function for enemy chase but it remains empty for now

--- GameJamProject/Assets/Scripts/Enemy.cs
+++ GameJamProject/Assets/Scripts/Enemy.cs
@@ -5,15 +5,31 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] Transform target;
+    [SerializeField] Animator enemyAnim;
+    public bool isChasing;
+    public bool isAttacking;
+    private EnemyHealth health;
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = GetComponent<EnemyHealth>();
+        enemyAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(target);
+        Chase();
+        Dead();
+    }
+
+    void Chase(){
+        
+    }
+    
+    void Dead(){
+        if(health.currentHealth > 0){
+            transform.LookAt(target);
+        }
     }
 }
--- GameJamProject/Assets/Scripts/EnemyHealth.cs
+++ GameJamProject/Assets/Scripts/EnemyHealth.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] int maxHealth = 100;
-    [SerializeField] int currentHealth;
+    public int currentHealth;
     [SerializeField] Animator animator;
     // Start is called before the first frame update
     void Start()
