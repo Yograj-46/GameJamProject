d6361fefcafb07ba21a4fdaac48ae58e74d2efcf
MUKESHK243 <mukesh.kumaran@outlook.com>
2024-02-23 21:44:06 +0530

    Treading and swiming animation

---
+++ GameJamProject/Assets/PlayerFloating.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class PlayerFloating : MonoBehaviour
+{
+    public float buoyancyForce = 50f;
+    private bool isInWater = false;
+    private Rigidbody rb;
+    Animator animator;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+       animator  = rb.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Water"))
+        {
+            isInWater = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Water"))
+        {
+            isInWater = false;
+            animator.SetBool("IsTreadingWater", false);
+
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isInWater)
+        {
+            animator.SetBool("IsTreadingWater", true);
+            Vector3 newPosition = rb.position + Vector3.up * buoyancyForce * Time.deltaTime;
+            rb.MovePosition(newPosition);
+        }
+    }
+}
--- GameJamProject/Assets/Scripts/Buoancy.cs
+++ GameJamProject/Assets/Scripts/Buoancy.cs
@@ -7,9 +7,11 @@ public class Buoancy : MonoBehaviour
     public float floatStrength;
     
     void OnTriggerStay(Collider other){
-        if(other.CompareTag("Player")){
+        if (other.CompareTag("Player"))
+        {
             Rigidbody rb = other.GetComponent<Rigidbody>();
-            if(rb != null){
+            if (rb != null)
+            {
                 //Calculate buoyancy force based on depth
                 float depth = Mathf.Abs(transform.position.y - other.transform.position.y);
                 float buoyancyForce = floatStrength * depth;
