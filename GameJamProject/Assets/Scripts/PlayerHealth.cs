ab5fe47b88cb9598bd4d3b0f51c8411bfb3de0c0
Jaguar704 <160331662+Jaguar704@users.noreply.github.com>
2024-03-01 10:41:15 +0530

    low health effect

--- GameJamProject/Assets/Scripts/PlayerHealth.cs
+++ GameJamProject/Assets/Scripts/PlayerHealth.cs
@@ -11,6 +11,10 @@ public class PlayerHealth : MonoBehaviour
     Animator animator;
     public bool isAlive = true;
 
+    public Image overlay;
+    public float overlaySpeed=1f;
+    float temp_A=0;
+
 
     private void Start()
     {
@@ -24,10 +28,27 @@ public class PlayerHealth : MonoBehaviour
     {
         UpdateDamage();
 
+        bool isLowhealth = healthSlider.value <= 12;
         if (healthSlider.value < minHealth)
             PlayerOut();
 
-        
+        if(isLowhealth && isAlive)
+        {
+            if(temp_A<100)
+            {
+                temp_A += Time.deltaTime*overlaySpeed;Debug.Log("+");
+            }
+            else
+            {
+                temp_A -= Time.deltaTime * overlaySpeed;Debug.Log("-");
+            }
+            
+            overlay.color = new Color(overlay.color.r, overlay.color.g, overlay.color.b, temp_A);
+        }
+        else
+        {
+            overlay.color = new Color(overlay.color.r, overlay.color.g, overlay.color.b, 0);
+        }
 
     }
 
